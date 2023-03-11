210073655ba5f87f7b38c2030a02b43c30d6ebe9
e0035165 <eeenginmaster95@gmail.com>
2023-03-11 16:13:36 +0800

    first commit

--- PlaneProblem/main.cpp
+++ PlaneProblem/main.cpp
@@ -6,9 +6,63 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <sstream>
+
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
     return 0;
+}
+
+
+
+
+int solution(int N, string &S) {
+    // Implement your solution here
+    int** rowLab = new int*[N];
+    for(int i=0;i<N;++i)
+    {
+        rowLab[i] = new int[10];
+        for(int j=0;j<10;++j)
+        {
+            rowLab[i][j] = 0;
+        }
+    }
+    string s;
+    stringstream ss(S);
+    vector<string>v;
+    while(getline(ss,s,' '))
+    {
+        v.push_back(s);
+    }
+    
+    for(auto x=v.begin();x!=v.end();++x)
+    {
+        if((*x)[1] == 'J' || (*x)[1] == 'K')
+        {
+            rowLab[(int)((*x)[0] - '1')][(int)((*x)[1] - 'A' - 1)] = 1;
+        } else {
+            rowLab[(int)((*x)[0] - '1')][(int)((*x)[1] - 'A')] = 1;
+        }
+    }
+
+    for(int i=0;i<N;++i)
+    {
+        for(int j=0;j<10;++j)
+        {
+            cout << rowLab[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    for(int i=0;i<N;++i)
+    {
+        delete[] rowLab[i];
+    }
+
+    delete[] rowLab;
+
+    return 1;
 }
