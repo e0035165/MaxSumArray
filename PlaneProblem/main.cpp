34c6401b431a8aafd9f6109aa3a068f8e9c7e229
e0035165 <eeenginmaster95@gmail.com>
2023-03-12 10:56:11 +0800

    second commit

--- PlaneProblem/main.cpp
+++ PlaneProblem/main.cpp
@@ -8,61 +8,109 @@
 #include <iostream>
 #include <vector>
 #include <sstream>
+#include <fstream>
 
 using namespace std;
+string ltrim(const string &str);
+string rtrim(const string &str);
+vector<string> split(const string &str);
+long maximumSum(vector<long>a, long m);
+
+
+
+
 
 int main(int argc, const char * argv[]) {
     
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string q_temp;
+    getline(cin, q_temp);
+
+    int q = stoi(ltrim(rtrim(q_temp)));
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        string first_multiple_input_temp;
+        getline(cin, first_multiple_input_temp);
+
+        vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+        int n = stoi(first_multiple_input[0]);
+
+        long m = stol(first_multiple_input[1]);
+
+        string a_temp_temp;
+        getline(cin, a_temp_temp);
+
+        vector<string> a_temp = split(rtrim(a_temp_temp));
+
+        vector<long> a(n);
+
+        for (int i = 0; i < n; i++) {
+            long a_item = stol(a_temp[i]);
+
+            a[i] = a_item;
+        }
+
+        long result = maximumSum(a, m);
+
+        cout << result << "\n";
+    }
+
+    fout.close();
+
     return 0;
 }
 
 
 
 
-int solution(int N, string &S) {
-    // Implement your solution here
-    int** rowLab = new int*[N];
-    for(int i=0;i<N;++i)
+long maximumSum(vector<long> a, long m) {
+    vector<long>sum((int)a.size());
+    sum[0] = a[0];
+    int itr = 1;
+    for(auto x=a.begin()+1;x!=a.end();++x)
     {
-        rowLab[i] = new int[10];
-        for(int j=0;j<10;++j)
-        {
-            rowLab[i][j] = 0;
-        }
-    }
-    string s;
-    stringstream ss(S);
-    vector<string>v;
-    while(getline(ss,s,' '))
-    {
-        v.push_back(s);
+        sum[itr] = (*x) + sum[itr-1];
+        sum[itr] = (sum[itr]%m);
+        ++itr;
     }
     
-    for(auto x=v.begin();x!=v.end();++x)
-    {
-        if((*x)[1] == 'J' || (*x)[1] == 'K')
-        {
-            rowLab[(int)((*x)[0] - '1')][(int)((*x)[1] - 'A' - 1)] = 1;
-        } else {
-            rowLab[(int)((*x)[0] - '1')][(int)((*x)[1] - 'A')] = 1;
-        }
+    return 0;
+}
+
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int c) {return !std::isspace(c);}));
+    return s;
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int c) {return !std::isspace(c);}));
+    return s;
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
     }
 
-    for(int i=0;i<N;++i)
-    {
-        for(int j=0;j<10;++j)
-        {
-            cout << rowLab[i][j] << " ";
-        }
-        cout << endl;
-    }
+    tokens.push_back(str.substr(start));
 
-    for(int i=0;i<N;++i)
-    {
-        delete[] rowLab[i];
-    }
-
-    delete[] rowLab;
-
-    return 1;
+    return tokens;
 }
