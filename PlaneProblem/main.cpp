cffd9001b44201c291045473a46dfee672725f07
e0035165 <eeenginmaster95@gmail.com>
2023-03-19 11:32:55 +0800

    committed fifth time

--- PlaneProblem/main.cpp
+++ PlaneProblem/main.cpp
@@ -22,22 +22,106 @@ string ltrim(const string &str);
 string rtrim(const string &str);
 vector<string> split(const string &str);
 long maximumSum(vector<long>a, long m);
-int CeilIndex(int* v, int l, int r, int key);
 
-int CeilIndex(int* v, int l, int r, int key)
+
+
+struct Node
 {
-    while (r - l > 1) {
-        int m = l + (r - l) / 2;
-        if (v[m] >= key)
-            r = m;
-        else
-            l = m;
+    long data;
+    Node* left = NULL;
+    Node* right = NULL;
+    vector<Node*>copies;
+    int height = 0;
+    Node(long data)
+    {
+        this->data = data;
+        this->height = 1;
     }
+    
 
-    return r;
+};
+
+void printNodal(Node* curr);
+bool sameNodeCheck(Node* newone, Node* currentOne);
+void setHeight(Node* curr);
+int getNodeBalanceFactor(Node* left, Node* right);
+
+bool sameNodeCheck(Node* newone, Node* currentOne)
+{
+    if(newone->data == currentOne->data)
+    {
+        currentOne->copies.push_back(newone);
+        return true;
+    } else {
+        return false;
+    }
+}
+
+void printNodal(Node* curr)
+{
+    if (curr == nullptr)
+    {
+        return;
+    }
+    printNodal(curr->left);
+
+    cout << curr->data << " ";
+
+    printNodal(curr->right);
+
+    return;
+}
+
+void setHeight(Node* curr)
+{
+    if (curr->right != NULL && curr->left != NULL)
+    {
+        curr->height = 1 + max(curr->right->height, curr->left->height);
+    }
+    else if (curr->right == NULL && curr->left != NULL)
+    {
+        curr->height = 1 + curr->left->height;
+    }
+    else if (curr->right != NULL && curr->left == NULL)
+    {
+        curr->height = 1 + curr->right->height;
+    }
+    else {
+        curr->height = 0;
+    }
+}
+
+int getNodeBalanceFactor(Node* left, Node* right)
+{
+    int lefter = left != NULL ? left->height : 0;
+    int righter = right != NULL ? right->height : 0;
+    return lefter - righter;
 }
 
 
+Node* insertNode(Node* newone, Node* currentOne)
+{
+    if(currentOne == NULL)
+    {
+        return newone;
+    }
+    
+    if(!sameNodeCheck(newone, currentOne))
+    {
+        if(newone->data < currentOne->data)
+        {
+            //cout << "left " << currentOne->data << endl;
+            currentOne->left = insertNode(newone, currentOne->left);
+        } else {
+            //cout << "right " << currentOne->data << endl;
+            currentOne->right = insertNode(newone, currentOne->right);
+        }
+    } else {
+        
+    }
+    
+    return currentOne;
+}
 
 
 int main(int argc, const char * argv[]) {
@@ -45,13 +129,13 @@ int main(int argc, const char * argv[]) {
     fstream input("File.txt", ios::in | ios::out);
 
     string q_temp;
-    getline(cin, q_temp);
+    getline(input, q_temp);
 
     int q = stoi(ltrim(rtrim(q_temp)));
-
+    cout << q << endl;
     for (int q_itr = 0; q_itr < q; q_itr++) {
         string first_multiple_input_temp;
-        getline(cin, first_multiple_input_temp);
+        getline(input, first_multiple_input_temp);
 
         vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
 
@@ -60,7 +144,7 @@ int main(int argc, const char * argv[]) {
         long m = stol(first_multiple_input[1]);
 
         string a_temp_temp;
-        getline(cin, a_temp_temp);
+        getline(input, a_temp_temp);
 
         vector<string> a_temp = split(rtrim(a_temp_temp));
 
@@ -74,7 +158,7 @@ int main(int argc, const char * argv[]) {
 
         long result = maximumSum(a, m);
 
-        cout << result << "\n";
+        //cout << result << "\n";
     }
 
     input.close();
@@ -97,28 +181,18 @@ long maximumSum(vector<long> a, long m) {
     }
     long max = LONG_MIN;
     long min = LONG_MAX;
-    long ans = 0;
-    long ans_two = 0;
-    vector<long>lister;
-    vector<vector<long>>horizontal_lister;
-    lister.push_back(sum[0]);
-    vector<long>buffer;
-    horizontal_lister.push_back(buffer);
-    for(int i=1;i<sum.size();++i)
+    
+    Node* start = new Node(sum[0]);
+
+    int i = 0;
+    while(++i < sum.size())
     {
-        if(lister[lister.size() - 1] < sum[i])
-        {
-            vector<long>buffer;
-            horizontal_lister.push_back(buffer);
-            lister.push_back(sum[i]);
-        } else {
-            
-        }
+        start = insertNode(new Node(sum[i]), start);
     }
     
-    
-    
-    
+    printNodal(start);
+    cout << endl;
+    cout << endl;
     
     return 0;
 }
